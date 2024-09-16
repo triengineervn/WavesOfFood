257f7e0ace620e56119c6e66f10ad9996f0d9165
Nguyễn Minh Trí <131839982+triengineervn@users.noreply.github.com>
2024-09-16 17:00:49 +0700

    implement search page, fix naming

--- app/src/main/java/com/example/wavesoffood/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/wavesoffood/fragments/SearchFragment.kt
@@ -1,27 +1,80 @@
 package com.example.wavesoffood.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wavesoffood.R
+import com.example.wavesoffood.adapters.MenuAdapter
+import com.example.wavesoffood.databinding.FragmentSearchBinding
+import com.example.wavesoffood.models.MenuItem
 
 class SearchFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentSearchBinding
+    private val originalFoodMenu = mutableListOf(
+        MenuItem("Pizza", "$11", R.drawable.menu_photo_1),
+        MenuItem("Burger", "$8", R.drawable.menu_photo_2),
+        MenuItem("Pasta", "$12", R.drawable.menu_photo_1),
+        MenuItem("Chicken", "$11", R.drawable.menu_photo_1),
+        MenuItem("Beef", "$18", R.drawable.menu_photo_1),
+        MenuItem("Fish", "$21", R.drawable.menu_photo_1),
+        MenuItem("Eggs", "$2", R.drawable.menu_photo_1)
+    )
+    private val filteredFoodMenu: MutableList<MenuItem> = originalFoodMenu.toMutableList()
 
+    private lateinit var adapter: MenuAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        adapter = MenuAdapter(filteredFoodMenu)
+        binding.menuRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.menuRecyclerView.adapter = adapter
+
+        setupSearchView()
+        showAllMenu()
+        return binding.root
     }
+
+    private fun showAllMenu() {
+        filteredFoodMenu.clear()
+        filteredFoodMenu.addAll(originalFoodMenu)
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setupSearchView() {
+        binding.searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener, android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                filterMenuItems(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                filterMenuItems(newText)
+                return true
+            }
+        })
+    }
+
+    private fun filterMenuItems(query: String) {
+        val filteredList = originalFoodMenu.filter {
+            it.name.contains(query, ignoreCase = true)
+        }
+
+        filteredFoodMenu.clear()
+        filteredFoodMenu.addAll(filteredList)
+        adapter.notifyDataSetChanged()
+    }
+
 
 }
